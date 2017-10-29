7c78f6b31ee350186fb440dced66e1664961c451
DimaGlushchenko <aws.inspiration@gmail.com>
2017-10-29 20:15:42 +0300

    ready

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -24,7 +24,7 @@ export class RegistrationComponent implements OnInit {
       email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
       name: new FormControl(null, [Validators.required]),
-      agree: new FormControl(false, [Validators.required])
+      agree: new FormControl(false, [Validators.requiredTrue])
     });
   }
 
