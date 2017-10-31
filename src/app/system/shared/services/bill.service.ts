85eadbc8a30dd9a081005cdee980ea7a1f512405
DimaGlushchenko <aws.inspiration@gmail.com>
2017-10-31 16:57:39 +0300

    ready

--- src/app/system/shared/services/bill.service.ts
+++ src/app/system/shared/services/bill.service.ts
@@ -1,22 +1,24 @@
-import { Response } from '@angular/http';
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Response } from "@angular/http";
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
 
-import { Bill } from '../models/bill.model';
+import { Bill } from "../models/bill.model";
+import { BaseApi } from "./../../../shared/core/base-api";
 
 @Injectable()
-export class BillService {
-  constructor(private http: Http) {}
+export class BillService extends BaseApi {
+  constructor(public http: Http) {
+    super(http);
+  }
 
   getBill(): Observable<Bill> {
-    return this.http.get('http://localhost:3000/bill')
-      .map((response: Response) => response.json());
+    return this.get("bill");
   }
 
-  getCurrency(base: string = 'RUB'): Observable<any> {
-    return this.http.get(`http://api.fixer.io/latest?base=${base}`)
+  getCurrency(base: string = "RUB"): Observable<any> {
+    return this.http
+      .get(`http://api.fixer.io/latest?base=${base}`)
       .map((response: Response) => response.json());
   }
-
 }
