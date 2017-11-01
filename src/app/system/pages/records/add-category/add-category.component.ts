ac7b5d270ae46884715335ba0cbe4379a553c2b1
DimaGlushchenko <aws.inspiration@gmail.com>
2017-11-01 16:22:50 +0200

    ready

--- src/app/system/pages/records/add-category/add-category.component.ts
+++ src/app/system/pages/records/add-category/add-category.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { Category } from "./../../../shared/models/category.model";
+import { CategoriesService } from "./../../../shared/services/categories.service";
 
 @Component({
-  selector: 'add-category',
-  templateUrl: './add-category.component.html',
-  styleUrls: ['./add-category.component.scss']
+  selector: "add-category",
+  templateUrl: "./add-category.component.html",
+  styleUrls: ["./add-category.component.scss"]
 })
 export class AddCategoryComponent implements OnInit {
 
-  constructor() { }
+  @Output() onCategoryAdd = new EventEmitter<Category>();
 
-  ngOnInit() {
+  constructor(private CategoriesService: CategoriesService) {}
+
+  ngOnInit() {}
+
+  onSubmit(form: NgForm) {
+    let { name, capacity } = form.value;
+    if (capacity < 0) capacity *= -1;
+
+    const category = new Category(name, capacity);
+
+    this.CategoriesService
+      .addCategory(category)
+      .subscribe((category: Category) => {
+        form.reset();
+        form.form.patchValue({capasyty: 1});
+        this.onCategoryAdd.emit(category);
+      });
   }
-
 }
--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -5,20 +5,22 @@ import { SharedModule } from "./../shared/shared.module";
 import { SystemRoutingModule } from "./system-routing.module";
 import { HistoryComponent } from "./pages/history/history.component";
 import { BillComponent } from "./pages/bill/bill.component";
+import { BillCardComponent } from './pages/bill/bill-card/bill-card.component';
 import { PlanningComponent } from "./pages/planning/planning.component";
 import { RecordsComponent } from "./pages/records/records.component";
 import { SystemComponent } from "./system/system.component";
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { SidebarComponent } from "./shared/components/sidebar/sidebar.component";
-
-import { DropdownDirective } from './shared/directives/dropdown.directive';
-import { BillCardComponent } from './pages/bill/bill-card/bill-card.component';
-import { BillService } from './shared/services/bill.service';
 import { CurrencyCardComponent } from './pages/bill/currency-card/currency-card.component';
-import { MomentPipe } from './shared/pipes/moment.pipe';
 import { AddEventComponent } from './pages/records/add-event/add-event.component';
 import { AddCategoryComponent } from './pages/records/add-category/add-category.component';
 import { EditCategoryComponent } from './pages/records/edit-category/edit-category.component';
+
+import { BillService } from './shared/services/bill.service';
+import { CategoriesService } from './shared/services/categories.service';
+import { MomentPipe } from './shared/pipes/moment.pipe';
+import { DropdownDirective } from './shared/directives/dropdown.directive';
+
 
 @NgModule({
   imports: [
@@ -42,6 +44,6 @@ import { EditCategoryComponent } from './pages/records/edit-category/edit-catego
     AddCategoryComponent,
     EditCategoryComponent
   ],
-  providers: [BillService]
+  providers: [BillService, CategoriesService]
 })
 export class SystemModule {}
