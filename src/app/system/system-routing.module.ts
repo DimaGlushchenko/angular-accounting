b7f18eac53530669e71ecc7eb3278efaac7b8e0f
DimaGlushchenko <aws.inspiration@gmail.com>
2018-02-17 19:48:54 +0200

    ready

--- src/app/system/system-routing.module.ts
+++ src/app/system/system-routing.module.ts
@@ -8,14 +8,16 @@ import { RecordsComponent } from './pages/records/records.component';
 import { SystemComponent } from './system/system.component';
 import { HistoryDetailComponent } from './pages/history/history-detail/history-detail.component';
 import { AuthGuard } from './../shared/services/auth.guard';
+import { GridComponent } from './pages/grid/grid.component';
 
 const routes: Routes = [
     {path: '', component: SystemComponent, canActivate: [AuthGuard], children: [
         {path: 'bill', component: BillComponent},
         {path: 'history', component: HistoryComponent},
+        {path: 'history/:id', component: HistoryDetailComponent},
         {path: 'planning', component: PlanningComponent},
         {path: 'records', component: RecordsComponent},
-        {path: 'history/:id', component: HistoryDetailComponent}
+        {path: 'grid', component: GridComponent},
     ]}
 ];
 
